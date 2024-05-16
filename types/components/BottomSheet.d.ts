25768202c99d5be00d1a35cb1a17697a4c2c2539
Haritha <harithsenevi4@gmail.com>
2024-05-16 18:21:51 +0100

    chore: Add types for BottomSheet component and provider

---
+++ types/components/BottomSheet.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { BottomSheetProps } from '../types/types';
+declare const BottomSheet: ({ isVisible, children, height, onHide }: BottomSheetProps) => React.JSX.Element;
+export default BottomSheet;
---
+++ types/types/types.d.ts
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from "react";
+export interface BottomSheetProps extends PropsWithChildren {
+    isVisible: boolean;
+    height?: number;
+    onHide: () => void;
+}
