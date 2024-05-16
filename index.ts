7e30581a7cfcaa2f8dcabb2a99fa3a2a9da4e90c
Haritha <harithsenevi4@gmail.com>
2024-05-16 15:34:21 +0100

    change package json

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import BottomSheetProvider from "./src/providers/BottomSheetProvider";
 import { useBottomSheet } from "./src/providers/BottomSheetProvider";
-import BottomSheet from "./src/components/BottomSheet";
 
-export { BottomSheetProvider, useBottomSheet, BottomSheet };
+export { BottomSheetProvider, useBottomSheet };
