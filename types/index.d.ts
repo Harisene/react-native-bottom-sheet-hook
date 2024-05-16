4219e7afe13b29920f35c95cce07990b764ff51b
Haritha <harithsenevi4@gmail.com>
2024-05-16 18:24:45 +0100

    chore: Add BottomSheet component and provider types, update gitignore

---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+import BottomSheetProvider from "./src/providers/BottomSheetProvider";
+import { useBottomSheet } from "./src/providers/BottomSheetProvider";
+export { BottomSheetProvider, useBottomSheet };
